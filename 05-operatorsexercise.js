ec6745af8afd957b6f7d986d90626cdf81ff8afa
luisemigoan <luisemigoan@hotmail.com>
2025-02-22 09:45:57 -0500

    Se resuelve primer parte del taller

---
+++ 05-operatorsexercise.js
@@ -0,0 +1,50 @@
+// 1. Crea una variable para cada operación aritmética
+let add = (5 + 4)
+let substract = (5 - 4)
+let multiplicacion = (5 * 4)
+let division = (5 / 4)
+let potencia = (5 ** 4)
+let residuo = (5 % 4)
+
+console.log(add)
+console.log(substract)
+console.log(multiplicacion)
+console.log(division)
+console.log(potencia)
+console.log(residuo)
+
+// 2. Crea una variable para cada tipo de operación de asignación,
+//    que haga uso de las variables utilizadas para las operaciones aritméticas
+add += 1
+console.log(add)
+
+substract -= 1
+console.log(substract)
+
+multiplicacion *= 2
+console.log(multiplicacion)
+
+division /= 1
+console.log(division)
+
+potencia **= 2
+console.log(potencia)
+
+residuo %= 5
+console.log(residuo)
+
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+
+// 5. Utiliza el operador lógico and
+
+// 6. Utiliza el operador lógico or
+
+// 7. Combina ambos operadores lógicos
+
+// 8. Añade alguna negación
+
+// 9. Utiliza el operador ternario
+
+// 10. Combina operadores aritméticos, de comparáción y lógicas
