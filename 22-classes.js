d9b1339dd87957601b3026267ed048a0f139f1f6
luisemigoan <luisemigoan@hotmail.com>
2025-03-02 22:14:55 -0500

    Se finaliza clase de clases

--- 22-classes.js
+++ 22-classes.js
@@ -72,6 +72,7 @@ class Person4 {
 const person6 = new Person4('Mike', 30, 'John Doe', 1000);
 console.log(person6.name);
 //console.log(person6.#bank); //Error, no se puede acceder a la propiedad privada.
+
 // Si se intenta acceder a una propiedad privada se lanzará un error, indicando que la propiedad no está definida, solamente la clase puede acceder a las propiedades privadas y solo dentro de la clase, podemos acceder a las propiedades privadas desde los métodos de la clase, pero no desde fuera de la clase, tampoco se puede asignar un nuevo valor a una propiedad privada desde fuera de la clase, finalmente esto lo que haría sería crear una nueva variable con el mismo nombre pero no modificaría la propiedad privada.
 
 // es posible dejar que se acceda a una propiedad privada, o que se pueda visualizar pero no modificar, para ello se pueden crear métodos get y set, estos métodos se utilizan para obtener y asignar valores a las propiedades de la clase, respectivamente, para definir un método get se escribe la palabra reservada get seguido del nombre del método, para definir un método set se escribe la palabra reservada set seguido del nombre del método, ambos métodos se definen como si fueran una propiedad, pero sin paréntesis, para acceder a una propiedad privada se utiliza la palabra reservada this seguido de un punto y el nombre de la propiedad.
@@ -104,6 +105,40 @@ console.log(person7.name);
 person7.bank = 2000;
 console.log(person7.bank);
 
+// Herencia en las clases, la herencia es una característica de la programación orientada a objetos que permite crear una clase a partir de otra clase, la clase que se hereda se conoce como clase padre o superclase, la clase que hereda se conoce como clase hija o subclase, la clase hija hereda todas las propiedades y métodos de la clase padre, además puede tener sus propias propiedades y métodos, para heredar una clase se utiliza la palabra reservada extends seguido del nombre de la clase padre, luego de la definición de la clase hija se llama al constructor de la clase padre utilizando la palabra reservada super seguido de paréntesis, dentro de los paréntesis se pasan los argumentos que recibe el constructor de la clase padre.
+class Person6 {
+    constructor(name, age, alias) {
+        this.name = name;
+        this.age = age;
+        this.alias = alias;
+    }
+    sayHello() {
+        console.log(`Hello, my name is ${this.name}`);
+    }
+}   
+class Employee extends Person6 {
+    constructor(name, age, alias, salary) {
+        super(name, age, alias);
+        this.salary = salary;
+    }
+    showSalary() {
+        console.log(`My salary is ${this.salary}`);
+    }
+}
+const employee1 = new Employee('John', 30, 'John Doe', 1000);
+employee1.sayHello();
+employee1.showSalary();
+
+// Métodos estáticos, los métodos estáticos son métodos que se pueden llamar sin necesidad de instanciar la clase, es decir, se pueden llamar directamente desde la clase, para definir un método estático se utiliza la palabra reservada static seguido del nombre del método, para llamar a un método estático se utiliza el nombre de la clase seguido de un punto y el nombre del método.
+class Math {    
+    static sum(a, b) {
+        return a + b;
+    }
+}
+console.log(Math.sum(5, 3));
+// Los métodos estáticos son útiles cuando se necesita un método que no dependa de las propiedades de la clase, es decir, un método que no necesite acceder a las propiedades de la clase, por ejemplo, un método que realice una operación matemática simple, en este caso no es necesario instanciar la clase para llamar al método, se puede llamar directamente desde la clase.
+
+// Cuando usar objetos y cuando clases, los objetos son útiles cuando se necesita crear un solo objeto con propiedades y métodos, por ejemplo, un objeto que represente un usuario, un objeto que represente un producto, etc., las clases son útiles cuando se necesita crear múltiples objetos con las mismas propiedades y métodos, por ejemplo, una clase que represente un usuario, una clase que represente un producto, etc., en resumen, si se necesita crear un solo objeto se puede utilizar un objeto, si se necesita crear múltiples objetos se puede utilizar una clase.
 
 
 
